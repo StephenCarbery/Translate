530625913d04edcbde5b1d35eb867249320a8aee
Stephen Carbery <carbery.stephen@gmail.com>
2017-03-02 23:41:46 +0000

    Wired up outlets, added Spanish and German as destination languages

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -8,30 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var textToTranslate: UITextView!
     @IBOutlet weak var translatedText: UITextView!
+    @IBOutlet weak var selectLanguage: UIPickerView!
+    
+    let langPicker = ["French", "Spanish", "German"]
+    var langSelect = ""
+    
     
     //var data = NSMutableData()
+    
+    //Number of columns of languages
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return langPicker.count
+    }
+    
+    //Recieves the specific row and specific component
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return langPicker[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        langSelect = langPicker[row]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.selectLanguage.delegate = self
+        self.selectLanguage.dataSource = self
+        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+       
     
     @IBAction func translate(_ sender: AnyObject) {
         
+        
         let str = textToTranslate.text
         let escapedStr = str?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
-        
-        let langStr = ("en|fr").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
-        
+        var langpair = ""
+        switch langSelect {
+            case "French" :
+                langpair = "en|fr"
+            case "German" :
+                langpair = "en|de"
+            case "Spanish" :
+                langpair = "en|es"
+            default :
+                langpair = "en|fr"
+        }
+        let langStr = (langpair).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         let urlStr:String = ("https://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
         
         let url = URL(string: urlStr)
